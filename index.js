541db1a5358aca1cf5f4bb5d198566a3c6740043
Kanha-1234 <161133758+Kanha-1234@users.noreply.github.com>
2024-12-06 18:03:51 +0530

    db connection success

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors"
-
+import dotenv from "dotenv"
+import connectDB from "./utils/db.js";
+dotenv.config({})
 
 
 const app = express();
@@ -20,5 +22,6 @@ app.use(cors(corsOptions))
 
 const PORT = process.env.PORT || 3030
 app.listen(PORT,()=>{
+    connectDB()
     console.log(`server listining on port ${PORT}`)
 })
